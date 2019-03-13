0ac282b5f464278d5405981794aef6a41adc5689
zhangruinan <ruinan.zhang@icloud.com>
2019-03-13 15:43:14 +0800

    fix simple sentiment typo

--- smoothnlp/simple/__init__.py
+++ smoothnlp/simple/__init__.py
@@ -1,4 +1,4 @@
 import smoothnlp
 from smoothnlp.jvm import LazyLoadingJClass,SafeJClass
 ner = LazyLoadingJClass("com.smoothnlp.nlp.simple.NormalizedNER")
-sentiment = LazyLoadingJClass("com.smoopythnlp.nlp.simple.SentimentAnalyzer")
+sentiment = LazyLoadingJClass("com.smoothnlp.nlp.simple.SentimentAnalyzer")
