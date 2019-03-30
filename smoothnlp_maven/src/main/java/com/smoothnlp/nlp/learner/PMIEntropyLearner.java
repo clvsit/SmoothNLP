e8bfe05b252035f744ebd48c05937de5bc1e59c6
Ruinan Zhang <victor@Ruinans-MacBook-Pro.local>
2019-03-30 14:34:35 +0700

    finish first version pmi entropy learner

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/learner/PMIEntropyLearner.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/learner/PMIEntropyLearner.java
@@ -1,9 +1,12 @@
 package com.smoothnlp.nlp.learner;
 
-
+import com.smoothnlp.nlp.SmoothNLP;
 import com.smoothnlp.nlp.util.PMIEntripyCalculator;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class PMIEntropyLearner extends BaseLearner {
 
@@ -12,7 +15,7 @@ public class PMIEntropyLearner extends BaseLearner {
 
     public PMIEntropyLearner(){
         this.pmicalc = new PMIEntripyCalculator();
-        this.computed = false
+        this.computed = false;
     }
 
     @Override
@@ -33,8 +36,34 @@ public class PMIEntropyLearner extends BaseLearner {
             computePMIE();
         }
         HashMap<String, HashMap<String,Double>> tokens2scores = this.pmicalc.getTokenScores4Sentence(inputText);
+        String[] inputTokens = SmoothNLP.segment_pipeline.segmentText(inputText);
+        int charIndex = 0;
+        ArrayList<HashMap<String,String>> resList = new ArrayList<HashMap<String, String>>();
+        for (int i = 0; i< inputTokens.length; i++){
+            HashMap<String,String> tokenRes = new HashMap<String, String>();
+            tokenRes.put("token",inputTokens[i]);
+            tokenRes.put("charStart",Integer.toString(charIndex));
+            charIndex+=inputTokens[i].length();
+            tokenRes.put("charEnd",Integer.toString(charIndex));
+            if (tokens2scores.containsKey(inputTokens[i])){
+                HashMap<String,Double> token2score = tokens2scores.get(inputTokens[i]);
+                for (String score_name: token2score.keySet()){
+                    tokenRes.put(score_name,Double.toString(token2score.get(score_name)));
+                }
+            }
+            resList.add(tokenRes);
+        }
+        Gson gsonobject = new Gson();
+        return gsonobject.toJson(resList);
+    }
 
-        return "";
+    public static void main(String[] args){
+        String sampletext1 = "五块钱虽然买不到多少";
+        String sampletext2 = "五块钱也算是钱";
+        PMIEntropyLearner pleaner = new PMIEntropyLearner();
+        pleaner.fit(sampletext1);
+        pleaner.fit(sampletext2);
+        System.out.println(pleaner.transform(sampletext1));
     }
 
 }
